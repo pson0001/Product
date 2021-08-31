c5cd6d5d9715c1350365a20130ab999ccba2803d
pson0007 <ping.song@monash.edu>
2021-08-31 23:04:44 +1000

    update home page

--- src/components/Notebook.js
+++ src/components/Notebook.js
@@ -3,7 +3,7 @@ import JupViewer from './jupView/JupView'
 const Notebook = () => {
   return (
     <div style={{ marginBottom: '100px' }}>
-      <JupViewer file="https://raw.githubusercontent.com/pson0001/CaseStudy/main/src/assets/influenza_au.ipynb"></JupViewer>
+      <JupViewer file="https://raw.githubusercontent.com/pson0001/Product/main/src/assets/influenza_au.ipynb"></JupViewer>
     </div>
   )
 }
